a55d9f1c640b0bafc78ab636cd57242e03f7687a
Baltrock <bengmmurphy@hotmail.com>
2024-05-31 20:16:19 +0100

    equals

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,10 @@
+
+describe Calculator do
+  context "Given two numbers" do
+    it "adds the numbers using the add method" do
+      calc = Calculator.new
+      sum = calc.add(2,3)
+      expect(sum).to eql(5)
+    end
+  end
+end
